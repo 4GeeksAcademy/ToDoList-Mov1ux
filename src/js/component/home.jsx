2cb730744489f6b4a05da826cadb07fcd946c2e3
Mov1ux <104890041+Mov1ux@users.noreply.github.com>
2024-02-16 19:20:47 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState }from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	const [tareas, setTareas] = useState([])
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5 border">
+			<h1>Tareas</h1>
+			<ul>
+				<li><input 
+					type="text"
+					value={inputValue}
+					onChange={(e) => 
+						 setInputValue(e.target.value)}
+					onKeyDown={(e)=> e.key == "Enter" ? setTareas (tareas.concat(inputValue)): null}
+					placeholder="Nuevas tareas" />
+					<button onClick={() =>{setTareas("")}}><i className="fa-solid fa-xmark"></i></button>
+					
+				</li>
+				{tareas.map((t)=>(
+				<li className="mt-1">
+					{t} <i className="fa-solid fa-xmark"></i>
+				</li>))}
+
+			</ul>
+			<div className="container  border-top mb-none">
+				<p>24 tareas</p>
+			</div>
 		</div>
 	);
 };
